33ad648946763e83584abf2fe1075103540f8feb
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2023-03-21 14:33:05 -0400

    working

--- C09/ex00/main.cpp
+++ C09/ex00/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
 
   if(argc != 2) {
-	   std::cout << "no input file provided" << std::endl;
+	   std::cout << "wrong number of argument" << std::endl;
     return 1;
   }
   try {
